b0f64f0c44ba761ae0dfa756482a9baa42302d6a
72 <72sevenzy2@gmail.com>
2026-04-05 22:48:50 +0500

    feat: add storing full notes in array (which acts like a database) through a click from the save btn

--- index.js
+++ index.js
@@ -8,7 +8,17 @@ let arr = []; // this is where ill store notes (for test cases)
 content.addEventListener("input", () => {
     if (content.value !== "") {
         arr.push(content.value);
-        console.log("pushed") // test case
-        console.log(arr) // test case
     }
 })
+
+// second task (appending the saved notes to a section in which the notes will appear)
+const save_btn = document.getElementById("save-btn"); // grabbing the save btn
+
+let storage = []; // storing our notes here (different from the arr array which is used for logging every key stroke made by the user)
+
+save_btn.addEventListener("click", () => {
+    if (content.value !== "") {
+        storage.push(content.value)
+        console.log(storage)
+    }
+});
